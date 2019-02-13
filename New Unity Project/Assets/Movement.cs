6705a4e0481eff9e256552acc4312aeb096cfb04
Mathias <mathdep38@sfr.fr>
2019-02-13 13:55:20 +0100

    Capture de drapeau

---
+++ New Unity Project/Assets/Movement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpSpeed = 8;
+    private Rigidbody rb;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity += jumpSpeed * Vector3.up;
+        }
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            if(other.name == "Blue flag")
+            {
+
+                rend.material.color = Color
+            }
+        }
+    }
+}
---
+++ New Unity Project/Assets/sc.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc : MonoBehaviour {
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 100, 15) * Time.deltaTime);
+    }
+}
